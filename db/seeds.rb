2ee55af2da0a8f2e0f44724be136fd3db3a710a2
SabrinaAEH <sabrina.aeh@gmail.com>
2024-04-25 16:50:49 +0200

    ajout des fichiers TGP

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+require 'faker'
+
+10.times do
+  city = City.create(
+    name: Faker::Address.city,
+    zip_code: Faker::Address.zip_code
+  )
+
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.unique.email,
+    description: Faker::Lorem.paragraph,
+    age: Faker::Number.between(from: 18, to: 100),
+    city: city
+  )
+end
+
+puts "seeds créés avec succès"
